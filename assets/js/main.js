7e07eb4a3506bbc54d3746a7e4cd7d4b617afbfd
gkasha <gialonkasha@gmail.com>
2020-05-31 16:31:44 -0400

    hsioefhs

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-(function($) {
+(function ($) {
 
 	var $window = $(window),
 		$body = $('body'),
@@ -14,197 +14,104 @@
 		settings = {
 
 			// Parallax background effect?
-				parallax: true,
+			parallax: true,
 
 			// Parallax factor (lower = more intense, higher = less intense).
-				parallaxFactor: 20
+			parallaxFactor: 20
 
 		};
 
 	// Breakpoints.
-		breakpoints({
-			xlarge:  [ '1281px',  '1800px' ],
-			large:   [ '981px',   '1280px' ],
-			medium:  [ '737px',   '980px'  ],
-			small:   [ '481px',   '736px'  ],
-			xsmall:  [ null,      '480px'  ],
-		});
+	breakpoints({
+		xlarge: ['1281px', '1800px'],
+		large: ['981px', '1280px'],
+		medium: ['737px', '980px'],
+		small: ['481px', '736px'],
+		xsmall: [null, '480px'],
+	});
 
 	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+	$window.on('load', function () {
+		window.setTimeout(function () {
+			$body.removeClass('is-preload');
+		}, 100);
+	});
 
 	// Touch?
-		if (browser.mobile) {
+	if (browser.mobile) {
 
-			// Turn on touch mode.
-				$body.addClass('is-touch');
+		// Turn on touch mode.
+		$body.addClass('is-touch');
 
-			// Height fix (mostly for iOS).
-				window.setTimeout(function() {
-					$window.scrollTop($window.scrollTop() + 1);
-				}, 0);
-
-		}
-
-	// Footer.
-		breakpoints.on('<=medium', function() {
-			$footer.insertAfter($main);
-		});
-
-		breakpoints.on('>medium', function() {
-			$footer.appendTo($header);
-		});
-
-	// Header.
-
-		// Parallax background.
-
-			// Disable parallax on IE (smooth scrolling is jerky), and on mobile platforms (= better performance).
-				if (browser.name == 'ie'
-				||	browser.mobile)
-					settings.parallax = false;
-
-			if (settings.parallax) {
-
-				breakpoints.on('<=medium', function() {
-
-					$window.off('scroll.strata_parallax');
-					$header.css('background-position', '');
-
-				});
-
-				breakpoints.on('>medium', function() {
-
-					$header.css('background-position', 'left 0px');
-
-					$window.on('scroll.strata_parallax', function() {
-						$header.css('background-position', 'left ' + (-1 * (parseInt($window.scrollTop()) / settings.parallaxFactor)) + 'px');
-					});
-
-				});
-
-				$window.on('load', function() {
-					$window.triggerHandler('scroll');
-				});
-
-			}
-
-	// Nav.
-	var $nav = $('#nav');
-
-	if ($nav.length > 0) {
-
-		// Shrink effect.
-		$main
-			.scrollex({
-				mode: 'top',
-				enter: function () {
-					$nav.addClass('alt');
-				},
-				leave: function () {
-					$nav.removeClass('alt');
-				},
-			});
-
-		// Links.
-		var $nav_a = $nav.find('a');
-
-		$nav_a
-			.scrolly({
-				speed: 1000,
-				offset: function () { return $nav.height(); }
-			})
-			.on('click', function () {
-
-				var $this = $(this);
-
-				// External link? Bail.
-				if ($this.attr('href').charAt(0) != '#')
-					return;
-
-				// Deactivate all links.
-				$nav_a
-					.removeClass('active')
-					.removeClass('active-locked');
-
-				// Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
-				$this
-					.addClass('active')
-					.addClass('active-locked');
-
-			})
-			.each(function () {
-
-				var $this = $(this),
-					id = $this.attr('href'),
-					$section = $(id);
-
-				// No section for this link? Bail.
-				if ($section.length < 1)
-					return;
-
-				// Scrollex.
-				$section.scrollex({
-					mode: 'middle',
-					initialize: function () {
-
-						// Deactivate section.
-						if (browser.canUse('transition'))
-							$section.addClass('inactive');
-
-					},
-					enter: function () {
-
-						// Activate section.
-						$section.removeClass('inactive');
-
-						// No locked links? Deactivate all links and activate this section's one.
-						if ($nav_a.filter('.active-locked').length == 0) {
-
-							$nav_a.removeClass('active');
-							$this.addClass('active');
-
-						}
-
-						// Otherwise, if this section's link is the one that's locked, unlock it.
-						else if ($this.hasClass('active-locked'))
-							$this.removeClass('active-locked');
-
-					}
-				});
-
-			});
+		// Height fix (mostly for iOS).
+		window.setTimeout(function () {
+			$window.scrollTop($window.scrollTop() + 1);
+		}, 0);
 
 	}
 
-	// Scrolly.
-	$('.scrolly').scrolly({
-		speed: 1000
+	// Footer.
+	breakpoints.on('<=medium', function () {
+		$footer.insertAfter($main);
 	});
+
+	breakpoints.on('>medium', function () {
+		$footer.appendTo($header);
+	});
+
+	// Header.
+
+	// Parallax background.
+
+	// Disable parallax on IE (smooth scrolling is jerky), and on mobile platforms (= better performance).
+	if (browser.name == 'ie'
+		|| browser.mobile)
+		settings.parallax = false;
+
+	if (settings.parallax) {
+
+		breakpoints.on('<=medium', function () {
+
+			$window.off('scroll.strata_parallax');
+			$header.css('background-position', '');
+
+		});
+
+		breakpoints.on('>medium', function () {
+
+			$header.css('background-position', 'left 0px');
+
+			$window.on('scroll.strata_parallax', function () {
+				$header.css('background-position', 'left ' + (-1 * (parseInt($window.scrollTop()) / settings.parallaxFactor)) + 'px');
+			});
+
+		});
+
+		$window.on('load', function () {
+			$window.triggerHandler('scroll');
+		});
+
+	}
 
 	// Main Sections: Two.
 
-		// Lightbox gallery.
-			$window.on('load', function() {
+	// Lightbox gallery.
+	$window.on('load', function () {
 
-				$('#three').poptrox({
-					caption: function($a) { return $a.next('h3').text(); },
-					overlayColor: '#2c2c2c',
-					overlayOpacity: 0.85,
-					popupCloserText: '',
-					popupLoaderText: '',
-					selector: '.work-item a.image',
-					usePopupCaption: true,
-					usePopupDefaultStyling: false,
-					usePopupEasyClose: false,
-					usePopupNav: true,
-					windowMargin: (breakpoints.active('<=small') ? 0 : 50)
-				});
+		$('#three').poptrox({
+			caption: function ($a) { return $a.next('h3').text(); },
+			overlayColor: '#2c2c2c',
+			overlayOpacity: 0.85,
+			popupCloserText: '',
+			popupLoaderText: '',
+			selector: '.work-item a.image',
+			usePopupCaption: true,
+			usePopupDefaultStyling: false,
+			usePopupEasyClose: false,
+			usePopupNav: true,
+			windowMargin: (breakpoints.active('<=small') ? 0 : 50)
+		});
 
-			});
+	});
 
-})(jQuery);
+})(jQuery); 
